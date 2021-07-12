560c525bf65b681a685ce0a705c5b33ed765e78a
Vivek Satikunvar <viveksatikunvar142@gmail.com>
2021-07-12 18:09:23 +0530

    build DocumentRow Component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Button from '@material-tailwind/react/Button'
 import Icon from '@material-tailwind/react/Icon'
 import Head from 'next/head'
 import Header from '../components/Header'
+import DocumentRow from '../components/DocumentRow'
 import Image from 'next/image'
 import { getSession, useSession } from 'next-auth/client'
 import Login from '../components/Login'
@@ -11,14 +12,19 @@ import ModalFooter from '@material-tailwind/react/ModalFooter'
 import { useState } from 'react'
 import { db } from '../firebase'
 import firebase from 'firebase'
+import { useCollectionOnce } from 'react-firebase-hooks/firestore'
+
 
 export default function Home() {
 
   const [session] = useSession();
+  
+  if(!session) return <Login />
+
   const [showModal,setShowModal] = useState(false);
   const [input,setInput] = useState("");
-
-  if(!session) return <Login />
+  const [snapshot] = useCollectionOnce(db.collection("userDocs").doc(session.user.email).collection("docs").orderBy("timestamp", "desc")
+  );
 
   const createDocument = () => {
     if(!input) return;
@@ -113,14 +119,29 @@ export default function Home() {
         </div>
       </section>
 
-      <section className='px-10 bg-white md:px-0'>
+      <section className='px-10 bg-white md:px-4'>
         <div className='max-w-3xl py-8 mx-auto text-sm text-gray-700'>
             <div className='flex items-center justify-between pb-5 '>
               <h2 className='flex-grow font-medium'>My Document</h2>
               <p className='mr-12 '>Date Created</p>
               <Icon name='folder' size='3xl' color='gray'/>
             </div>
-        </div>
+        
+
+
+      {snapshot?.docs.map((doc) => (
+        <DocumentRow
+          key={doc.id}
+          id={doc.id}
+          fileName={doc.data().fileName}
+          date={doc.data().timestamp}
+        />
+
+
+
+      ))}
+
+      </div>    
       </section>
     </div>
   )
